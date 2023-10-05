c3cbc96440d465249e56950f8f4e3791c0e65350
Avinash14102003 <120013515+Avinash14102003@users.noreply.github.com>
2023-10-05 12:35:05 +0530

    Add files via upload

---
+++ Day13/Repository/repository.java
@@ -0,0 +1,15 @@
+package com.team7.t7.Repository;
+
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.team7.t7.Entity.Bookmystay;
+
+
+public interface repository extends JpaRepository<Bookmystay,Long> {
+
+	
+
+}
