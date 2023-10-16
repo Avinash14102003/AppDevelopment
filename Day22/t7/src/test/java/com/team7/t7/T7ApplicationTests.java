75d97c2dcd381c6fc6d069cb91d2a603f42ae801
Avinash14102003 <120013515+Avinash14102003@users.noreply.github.com>
2023-10-16 14:00:00 +0530

    Add files via upload

---
+++ Day22/t7/src/test/java/com/team7/t7/T7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team7.t7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
